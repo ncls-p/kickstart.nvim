a2bfafe4c176e44d9c840d89ac445835783073de
Nicolas Pierrot <nicolas@archipels.io>
2025-03-15 13:55:07 +0100

    feat(plugins): add CopilotChat.nvim plugin
    Replaced olimorris/codecompanion.nvim with CopilotC-Nvim/CopilotChat.nvim.
    Updated dependencies and build instructions accordingly.
    
    - Added github/copilot.vim and nvim-lua/plenary.nvim as dependencies
    - Included build step for tiktoken on MacOS or Linux

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,23 +7,15 @@ return {
   'github/copilot.vim',
 
   -- Copilot Chat
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   dependencies = {
-  --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-  --     { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-  --   },
-  --   build = 'make tiktoken', -- Only on MacOS or Linux
-  --   opts = {
-  --     -- See Configuration section for options
-  --   },
-  -- },
   {
-    'olimorris/codecompanion.nvim',
-    config = true,
+    'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
     },
   },
 
